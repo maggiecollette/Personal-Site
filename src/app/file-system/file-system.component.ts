279ef96cfbbfea3b082c75c13d8788c71a31d317
Elizabeth Jamison <jamison.e@northeastern.edu>
2023-10-12 14:31:54 -0400

    Fall 2023 Updates: resume, photos, new projects (fixed commit that came from elizabeth's account)

--- src/app/file-system/file-system.component.ts
+++ src/app/file-system/file-system.component.ts
@@ -9,7 +9,7 @@ export class FileSystemComponent {
   name = "File System";
   github = '';
   description = [
-    "For this project, we built a FUSE filesystem that allows you to mount a 1 MB disc image as a filesystem.",
+    "For this project, we built a FUSE filesystem that allows a user to mount a 1 MB disc image as a filesystem.",
     "The main goal of this project was to allow for the creation and deletion of variously sized files, properly " +
     "allocating and deallocating space as the a file grows and shrinks.",
     "We first implemented functionality for creating files with names at least 10 characters long, then the ability " +
